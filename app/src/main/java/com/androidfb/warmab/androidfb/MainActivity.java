533df283f6e8ed00cfae250eaa1ac6e78608f6fc
Alonso Salcido <alonsosalcido13@gmail.com>
2017-05-09 17:53:37 -0600

    listener of lblCielo data

--- app/src/main/java/com/androidfb/warmab/androidfb/MainActivity.java
+++ app/src/main/java/com/androidfb/warmab/androidfb/MainActivity.java
@@ -2,12 +2,52 @@ package com.androidfb.warmab.androidfb;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAGLOG = "firebase-db";
+
+    private TextView lblCielo;
+    private TextView lblTemperatura;
+    private TextView lblHumedad;
+    private Button btnEliminarListener;
+
+    private DatabaseReference dbCielo;
+    private DatabaseReference dbPrediccion;
+    private ValueEventListener eventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lblCielo = (TextView)findViewById(R.id.lblCielo);
+        lblTemperatura = (TextView)findViewById(R.id.lblTemperatura);
+        lblHumedad = (TextView)findViewById(R.id.lblHumedad);
+        btnEliminarListener = (Button)findViewById(R.id.btnEliminarListener);
+
+        DatabaseReference dbCielo = FirebaseDatabase.getInstance()
+                .getReference().child("prediccion-hoy").child("cielo");
+
+        dbCielo.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String valor = dataSnapshot.getValue().toString();
+                lblCielo.setText(valor);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAGLOG, "Error!", databaseError.toException());
+            }
+        });
     }
 }
